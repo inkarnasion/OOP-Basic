ca6c9ab265cc7510e5123e107868d5fbb8ba08ea
inkarnasion <todor8009@gmail.com>
2018-06-28 17:20:33 +0300

    Exercises
    Small changes in task

--- Encapsulation/Exercises/06. Football Team Generator/Main.java
+++ Encapsulation/Exercises/06. Football Team Generator/Main.java
@@ -17,32 +17,31 @@ public class Main {
             String[] command = reader.readLine().split(";");
             if ("END".equals(command[0]))break;
             try {
-                switch (command[0]) {
-                    case "Team":
-                        teams.put(command[1], new Team(command[1]));
-                        break;
-                    case "Rating":
-                        if (teams.containsKey(command[1])) {
-                            System.out.println(teams.get(command[1]));
-                        } else {
-                            System.out.println("Team " + command[1] + " does not exist.");
-                        }
-                        break;
-                    case "Add":
-                        if (teams.containsKey(command[1])) {
-                            teams.get(command[1]).addPlayer(new Player(command[2], Double.valueOf(command[3]), Double.valueOf(command[4]),
-                                    Double.valueOf(command[5]), Double.valueOf(command[6]), Double.valueOf(command[7])));
-                        } else {
-                            System.out.println("Team " + command[1] + " does not exist.");
-                        }
-                        break;
-                    case "Remove":
-                        if (teams.containsKey(command[1])) {
-                            teams.get(command[1]).removePlayer(command[2]);
-                        } else {
-                            System.out.println("Team " + command[1] + " does not exist.");
-                        }
-                        break;
+                if ("Team".equals(command[0])) {
+                    teams.put(command[1], new Team(command[1]));
+
+                } else if ("Rating".equals(command[0])) {
+                    if (teams.containsKey(command[1])) {
+                        System.out.println(teams.get(command[1]));
+                    } else {
+                        System.out.println("Team " + command[1] + " does not exist.");
+                    }
+
+                } else if ("Add".equals(command[0])) {
+                    if (teams.containsKey(command[1])) {
+                        teams.get(command[1]).addPlayer(new Player(command[2], Double.valueOf(command[3]), Double.valueOf(command[4]),
+                                Double.valueOf(command[5]), Double.valueOf(command[6]), Double.valueOf(command[7])));
+                    } else {
+                        System.out.println("Team " + command[1] + " does not exist.");
+                    }
+
+                } else if ("Remove".equals(command[0])) {
+                    if (teams.containsKey(command[1])) {
+                        teams.get(command[1]).removePlayer(command[2]);
+                    } else {
+                        System.out.println("Team " + command[1] + " does not exist.");
+                    }
+
                 }
             } catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
